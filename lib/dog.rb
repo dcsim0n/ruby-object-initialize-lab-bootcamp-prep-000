c3f2e856c1aa7f705e96a0edb0f178a90719b46f
Dana Simmons <dsanalog@gmail.com>
2019-01-17 20:04:25 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  def initialize(breed_is)
+    @breed = breed_is
+  end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  
+  def breed=(breed)
+    @breed = breed
+  end
+end
